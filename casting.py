62d169fc387d2744043ded7b0e4f0c72efc71dc8
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-08 11:06:20 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,18 @@
+Name="cat"
+age=28
+weight=30.67
+sudent=True
+print(Name)
+print("type of name",type(Name))
+print(age)
+print("Type of age",type(age))
+print(weight)
+print("type of weight",type(weight))
+print(sudent)
+print("type of sudent",type(sudent))
+age=str(age)
+print(age)
+print("Type of age",type(age))
+weight=int(weight)
+print(weight)
+print("type of weight",type(weight))
