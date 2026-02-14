ddf1b70f9b87f0889fd234ac6207d434029b2a42
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-14 12:43:55 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,8 @@
+math=int(input("marks in math: "))
+sci=int(input("marks in sci: "))
+eng=int(input("marks in eng: "))
+hindi=int(input("marks in hindi "))
+print("the percentage of all the marks are ")
+sum=sci+eng+math+hindi
+percentage=(sum/400)*100
+print(percentage)
