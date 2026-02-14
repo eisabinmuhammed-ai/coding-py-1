a82e8dc4877f893869efed90c7ab6be263ac7cde
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-14 12:30:50 +0530

    Add files via upload

---
+++ count the lons.py
@@ -0,0 +1,7 @@
+Amount=(int(input("pls enter the amout for whithdraw: ")))
+note1=Amount//100
+note2=(Amount%100)//50
+note3=((Amount%100)%50)//10
+print("notes of 100",note1)
+print("notes of 50",note2)
+print("notes of 10",note3)
