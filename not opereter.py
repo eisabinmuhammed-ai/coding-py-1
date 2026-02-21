66368a8f7695f62beb0378d72284d4cb7c4eb7aa
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-21 12:38:23 +0530

    Add files via upload

---
+++ not opereter.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+print(a != b)
+print(a != b)
+a="python"
+b="coding"
+if a != b :
+    print(a,"and",b, "are diffrent")
+a=4
+b=5
+if (a==1) != (b==5):
+    print("hello")
+b=int(input("enter number: " ))
+if b%2 != 0:
+    print("it is not a even number")
+else:
+    print("it is a even number")    
