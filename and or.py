d5a4fde2d0a9b595f3dfb48427337e37da49c1e5
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-21 12:25:02 +0530

    Add files via upload

---
+++ and or.py
@@ -0,0 +1,19 @@
+a=10
+c=12
+b=0
+if a and b and c:
+    print("all the numbers are grater than 0")
+else:
+    print("at lest one of the numbers is not grater than 0")
+ 
+b=-10
+c=12
+if a>0 or b>0:
+    print("one of the the numbers is grater than 0")
+else:
+    print("non of the numbers is grater than 0")
+if  c>0 or b>0:   
+    print("one of the the numbers is grater than 0")
+else:
+    print("non of the numbers is grater than 0")
+
