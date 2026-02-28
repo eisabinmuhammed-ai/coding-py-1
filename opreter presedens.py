2dedf6fdc98f71adae20877175f04f9938d3fce2
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-28 12:19:54 +0530

    Add files via upload

---
+++ opreter presedens.py
@@ -0,0 +1,13 @@
+v=6
+w=5
+x=9
+y=7
+z=0
+z=(v+w)*x/y;
+print("(v+w)*x/y is =",z)
+name="ayyoob"
+age=4
+if name=="ayyoob"or name=="max" and age >=3:
+    print("hello")
+else:
+    print("goodbye")
