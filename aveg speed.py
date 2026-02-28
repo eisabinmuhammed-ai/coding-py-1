86b48b2cbf6d7157c21438e96371db85a5826ffa
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-02-28 12:54:15 +0530

    Add files via upload

---
+++ aveg speed.py
@@ -0,0 +1,12 @@
+a=int(input("enter number: "))
+b=int(input("enter number: "))
+c=int(input("enter number: "))
+avg=a+b+c/3
+if a>=avg :
+    print("a is Grater than avg")
+elif b>avg :
+    print("b is Grater than avg")
+elif c>avg:
+    print("c is Grater than avg")
+else:
+    print("invaled input")
