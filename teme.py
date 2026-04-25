9faa4f2df9caaff5831fd4be1755199c53dcc15e
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-04-25 12:22:47 +0530

    Add files via upload

---
+++ teme.py
@@ -0,0 +1,5 @@
+from datetime  import datetime,date,time
+today=datetime.today()
+time=datetime.now()
+print("todays date is ",today,"todays time is ",time)
+print("datecompenents",today.year,"/",today.month,"/",today.day)
