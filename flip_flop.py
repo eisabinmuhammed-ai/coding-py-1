98239c3e2e8687942886555c3cf098626ebc78fd
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-05-02 12:32:48 +0530

    Add files via upload

---
+++ flip_flop.py
@@ -0,0 +1,14 @@
+def paindr(r):
+    e=len(r)-1
+    s=0
+    while(s<e):
+        if (r[s]!=r[e]):
+            return False
+        s=s+1
+        e=e-1
+    return True
+r=(1,2,3,4,5,3,2,1)
+if (paindr(r)):
+    print("it is a flip flop")
+else:
+    print("it is not a flip flop")
