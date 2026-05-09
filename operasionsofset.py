1c7cbe82bd7dc3c549b728623d132869157a5169
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-05-09 12:17:20 +0530

    Add files via upload

---
+++ operasionsofset.py
@@ -0,0 +1,12 @@
+my_set={1,2,3}
+print(my_set)
+my_set={1.0,"hello",1,4,3}
+print(my_set)
+my_set={1,2,3,3,4,3,2,4,4,1,1,1}
+print(my_set)
+my_set2=set([2,4,5,5,4])
+print(my_set2)
+num_set={0,1,2,3,4,5}
+print("first set is ",num_set)
+num_set.pop()
+print(num_set)
